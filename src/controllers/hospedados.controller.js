f6ce5da340f2a46473176ef3b823f060101c8d3a
VictoriaMartinSahagun <vams.00@hotmail.com>
2022-06-23 20:31:22 -0300

    Correccion id

--- src/controllers/hospedados.controller.js
+++ src/controllers/hospedados.controller.js
@@ -5,7 +5,7 @@ const getCabanaByIdHuesped = async (req, res) => {
     if(!isNaN(id)){
         const cabana = await db.query('SELECT cabana_id FROM hospedados WHERE huesped_id = $1',[id]);
         if(cabana.rows.length > 0){
-            const response = await db.query('SELECT capacidad,numero FROM cabanas WHERE cabana_id = $1',[cabana.rows[0].cabana_id]);
+            const response = await db.query('SELECT capacidad,numero FROM cabanas WHERE id = $1',[cabana.rows[0].cabana_id]);
             res.status(200).json(response.rows[0]);
         }else{
             res.status(404).json({error: 'not found'});
