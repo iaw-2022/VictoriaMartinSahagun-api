75e3d35a424b37e513819eb4df7f91755f0181f3
VictoriaMartinSahagun <vams.00@hotmail.com>
2022-06-27 16:42:48 -0300

    correccion

--- src/controllers/actividades.controller.js
+++ src/controllers/actividades.controller.js
@@ -46,7 +46,7 @@ const getActividadesSinReservaByHuespedId = async (req, res) => {
         const cabana = await db.query('SELECT cabana_id FROM hospedados WHERE huesped_id = $1',[id]);
         const response = await db.query(
             'SELECT id,nombre,descripcion,horario,localizacion,img FROM (SELECT actividades.id as id,nombre,descripcion,horario,localizacion,img,reservas_actividades.id as cabana_id  FROM actividades JOIN reservas_actividades ON reservas_actividades.actividad_id=actividades.id) as actividades_reservas WHERE cabana_id!=$1'
-            ,[cabana.rows[0].cabana_id])
+            ,[cabana.rows[0].cabana_id]);
 
         if(response.rows.length > 0){
             res.status(200).json(response.rows);
